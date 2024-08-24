640a023c8152f32235b04909a07aca34a0a1762c
Taeho4 <138271363+Taeho4@users.noreply.github.com>
2024-08-24 10:15:38 +0900

    Update front.jsx

--- front.jsx
+++ front.jsx
@@ -8,7 +8,7 @@ function App() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await fetch('http://localhost:5000/api/query', {
+      const res = await fetch('http://localhost:8000/api/query', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
